c20341dbebd3495376d5043bd5aa18d5df5f7870
Pedro Brandão <phbrandao1206@gmial.com>
2021-02-14 19:39:17 +0100

    Add route authentication and redirect

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -46,8 +46,9 @@ const SignIn: React.FC = () => {
                 if (err instanceof Yup.ValidationError) {
                     const errors = getValidationErrors(err);
                     formRef.current?.setErrors(errors);
-                    return;
+                    // return;
                 }
+
                 addToast({
                     type: 'error',
                     title: 'Authentication error',
